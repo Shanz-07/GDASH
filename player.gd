05eea80567e7ec5ded1f83e14c19db7dfaaf6ef4
Shanz-07 <nouman8c@gmail.com>
2024-01-11 17:33:48 +0530

    v14

--- player.gd
+++ player.gd
@@ -2,25 +2,26 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*5
 var jump_speed = 900
 var count=1
-var target_angle=180
+var target_angle=0
 var rotate0=true
-
+var fly = false
 func _ready():
 	pass
 func _process(delta):
-	if not is_on_floor():
+	print(Engine.get_frames_per_second())
+	print(fly)
+	if not is_on_floor() and fly==false:
 		velocity.y += gravity * delta
-
-		
+	if Input.is_action_pressed("fly") and fly == true:
+		velocity.y+=-7500*delta
+	if fly==true:
+		velocity.y += gravity * delta*0.5
+	move_and_slide()
 	position.x+=700*delta
-	if is_on_floor() and Input.is_action_pressed("up") :
-		
-		velocity.y +=-jump_speed
-		
-		print($".".position.y)
+	if is_on_floor() and Input.is_action_pressed("up") and fly == false:
+		velocity.y =-jump_speed
 	move_and_slide()
 	
-
 func _on_areaa_body_entered(body):
 	if body.is_in_group("spikes"):
 		position=Vector2(100,139)
@@ -33,14 +34,9 @@ func _on_areaa_body_entered(body):
 		
 		print("yes")
 	pass # Replace with function body.
-
-
-
-
-
 func _on_area_2d_2_body_entered(body):
-	print('fffafsafasfasf')
-	if Input.is_action_pressed("fly"):
-		position.y-=200*get_physics_process_delta_time()
-		
+	fly=true	
+	pass # Replace with function body.
+func _on_area_2d_2_body_exited(body):
+	fly=false
 	pass # Replace with function body.
--- test.gd
+++ test.gd
@@ -1,27 +1,51 @@
 extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*5
 var jump_speed = 900
+var count=1
+var target_angle=180
+var rotate0=true
+var fly = false
+var rotation_progress=0
+var rotation_speed=180
 func _ready():
 	pass
 func _process(delta):
-	if not is_on_floor():
+	print(fly)
+	if not is_on_floor() and fly==false:
 		velocity.y += gravity * delta
-	$".".position.x+=700*delta
-	if is_on_floor() and Input.is_action_pressed("up") and $".".rotation_degrees==0:
-		var tween=create_tween()
-		tween.tween_property($".","rotation",deg_to_rad(180),0.5)
-		velocity.y = -jump_speed
-		print($".".position.y)
+	if Input.is_action_pressed("fly") and fly == true:
+		velocity.y+=-7500*delta
+	if fly==true:
+		velocity.y += gravity * delta*0.5
 	move_and_slide()
-	if is_on_floor() and Input.is_action_pressed("up"):
-		var tween=create_tween()
-		tween.tween_property($".","rotation",deg_to_rad(0),0.5)
-		velocity.y =-jump_speed*1.38
-	move_and_slide()
-func _on_area_body_entered(body):
-	if body is CharacterBody2D:
-		position=Vector2(100,139)
-func _on_area_2d_body_entered(body):
-	if body is CharacterBody2D:
-		position=Vector2(100,139)
+	
+	if is_on_floor() and Input.is_action_pressed("up") and $".".rotation_degrees == 0 and fly == false:
+		
+		rotation_progress += rotation_speed * delta
+		rotation_progress = min(rotation_progress, 180)
+		$".".rotate(deg_to_rad( rotation_speed * delta))
+		if rotation_progress >= 180:
+			velocity.y = -jump_speed
+			rotation_progress = 0 
+		
 
+	  
+			
+func _on_areaa_body_entered(body):
+	if body.is_in_group("spikes"):
+		position=Vector2(100,139)
+		$"../Label".set_text("Attempt "+str(count))
+		count+=1
+		var tween = get_tree().create_tween()
+		tween.tween_property($"../ParallaxBackground/ParallaxLayer/Background-2", "modulate", Color("0096ff",1), 1)
+		var tween1 = get_tree().create_tween()
+		tween1.tween_property($"../ParallaxBackground2/ParallaxLayer/Ground-long", "modulate", Color.BLUE, 1)
+		
+		print("yes")
+	pass # Replace with function body.
+func _on_area_2d_2_body_entered(body):
+	fly=true	
+	pass # Replace with function body.
+func _on_area_2d_2_body_exited(body):
+	fly=false
+	pass # Replace with function body.
